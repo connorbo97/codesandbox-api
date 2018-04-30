45d4d8631b3672ddeea21cd8777752cf7e04eac3
Jehu Lee <connorbo97@g.ucla.edu>
2018-04-30 14:56:06 -0700

    comments and unfurl structure

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const deleteSandbox = require('./lib/deleteSandbox');
 const resources = require('./lib/resources');
 const deleteResource = require('./lib/deleteResource');
 const forkURL = require('./lib/forkURL');
+const unfurlURL = require('./lib/unfurlURL');
 
 const createSandbox = require('./lib/createSandbox');
 
@@ -30,22 +31,22 @@ app.use(bodyParser.json())
 // const DIR = __dirname + '/projects'
 
 
-// app.get("/api/v1/test", upload.array(), async function (req, res) {
-//   console.log(`======Creating new sandbox start=======\n`)
-//   console.time('createSand')
-//   let data = await createSandbox();
+app.get("/api/v1/test", upload.array(), async function (req, res) {
+  console.log(`======Creating new sandbox start=======\n`)
+  console.time('createSand')
+  let data = await createSandbox();
 
-//   console.timeEnd('createSand')
-//   console.log(`======Creating new sandbox start=======\n`)
+  console.timeEnd('createSand')
+  console.log(`======Creating new sandbox start=======\n`)
 
-//   // return res.status(200).send({"data":{"view_count":322117,"version":32,"user_liked":false,"updated_at":"2018-04-11T12:55:29.664977","title":null,"template":"create-react-app","tags":[],"source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","privacy":0,"owned":false,"original_git_commit_sha":null,"original_git":null,"npm_dependencies":{"react-dom":"16.0.0","react":"16.0.0"},"modules":[{"title":"package.json","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"ZGQK6","is_binary":false,"id":"dd3f0f6a-4555-457f-b2af-963bf00f9172","directory_shortid":null,"code":"{\n  \"name\": \"new\",\n  \"version\": \"1.0.0\",\n  \"description\": \"\",\n  \"keywords\": [],\n  \"main\": \"src/index.js\",\n  \"dependencies\": {\n    \"react\": \"16.2.0\",\n    \"react-dom\": \"16.2.0\",\n    \"react-scripts\": \"1.1.0\"\n  },\n  \"devDependencies\": {},\n  \"scripts\": {\n    \"start\": \"react-scripts start\",\n    \"build\": \"react-scripts build\",\n    \"test\": \"react-scripts test --env=jsdom\",\n    \"eject\": \"react-scripts eject\"\n  }\n}\n"},{"title":"Hello.js","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"NKZ4K","is_binary":false,"id":"7546d2c4-6657-46d9-ba63-8564e0dcb463","directory_shortid":"GXOoy","code":"import React from 'react';\n\nexport default ({ name }) => <h1>Hello {name}!</h1>;\n"},{"title":"index.js","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"wRo98","is_binary":false,"id":"928871a1-bbdc-425c-ace2-0b302b14a58a","directory_shortid":"GXOoy","code":"import React from 'react';\nimport { render } from 'react-dom';\nimport Hello from './Hello';\n\nconst styles = {\n  fontFamily: 'sans-serif',\n  textAlign: 'center',\n};\n\nconst App = () => (\n  <div style={styles}>\n    <Hello name=\"CodeSandbox\" />\n    <h2>Start editing to see some magic happen {'\\u2728'}</h2>\n  </div>\n);\n\nrender(<App />, document.getElementById('root'));\n"},{"title":"index.html","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"BA1N","is_binary":false,"id":"9c54d8d0-5a0e-4e5f-8794-3092757733ee","directory_shortid":"rgkK4","code":"<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n\t<meta charset=\"utf-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n\t<meta name=\"theme-color\" content=\"#000000\">\n\t<!--\n      manifest.json provides metadata used when your web app is added to the\n      homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/\n    -->\n\t<link rel=\"manifest\" href=\"%PUBLIC_URL%/manifest.json\">\n\t<link rel=\"shortcut icon\" href=\"%PUBLIC_URL%/favicon.ico\">\n\t<!--\n      Notice the use of %PUBLIC_URL% in the tags above.\n      It will be replaced with the URL of the `public` folder during the build.\n      Only files inside the `public` folder can be referenced from the HTML.\n\n      Unlike \"/favicon.ico\" or \"favicon.ico\", \"%PUBLIC_URL%/favicon.ico\" will\n      work correctly both with client-side routing and a non-root public URL.\n      Learn how to configure a non-root public URL by running `npm run build`.\n    -->\n\t<title>React App</title>\n</head>\n\n<body>\n\t<noscript>\n\t\tYou need to enable JavaScript to run this app.\n\t</noscript>\n\t<div id=\"root\"></div>\n\t<!--\n      This HTML file is a template.\n      If you open it directly in the browser, you will see an empty page.\n\n      You can add webfonts, meta tags, or analytics to this file.\n      The build step will place the bundled scripts into the <body> tag.\n\n      To begin the development, run `npm start` or `yarn start`.\n      To create a production bundle, use `npm run build` or `yarn build`.\n    -->\n</body>\n\n</html>"}],"like_count":211,"id":"new","git":null,"forked_from_sandbox":null,"fork_count":0,"external_resources":[],"entry":"src/index.js","directories":[{"title":"src","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"GXOoy","id":"d27aefca-c15c-41a1-b9d5-bd362fdd7f19","directory_shortid":null},{"title":"public","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"rgkK4","id":"859f77fe-8e09-4efb-bd44-f66ea4f949e4","directory_shortid":null}],"description":null,"author":null}})
+  // return res.status(200).send({"data":{"view_count":322117,"version":32,"user_liked":false,"updated_at":"2018-04-11T12:55:29.664977","title":null,"template":"create-react-app","tags":[],"source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","privacy":0,"owned":false,"original_git_commit_sha":null,"original_git":null,"npm_dependencies":{"react-dom":"16.0.0","react":"16.0.0"},"modules":[{"title":"package.json","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"ZGQK6","is_binary":false,"id":"dd3f0f6a-4555-457f-b2af-963bf00f9172","directory_shortid":null,"code":"{\n  \"name\": \"new\",\n  \"version\": \"1.0.0\",\n  \"description\": \"\",\n  \"keywords\": [],\n  \"main\": \"src/index.js\",\n  \"dependencies\": {\n    \"react\": \"16.2.0\",\n    \"react-dom\": \"16.2.0\",\n    \"react-scripts\": \"1.1.0\"\n  },\n  \"devDependencies\": {},\n  \"scripts\": {\n    \"start\": \"react-scripts start\",\n    \"build\": \"react-scripts build\",\n    \"test\": \"react-scripts test --env=jsdom\",\n    \"eject\": \"react-scripts eject\"\n  }\n}\n"},{"title":"Hello.js","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"NKZ4K","is_binary":false,"id":"7546d2c4-6657-46d9-ba63-8564e0dcb463","directory_shortid":"GXOoy","code":"import React from 'react';\n\nexport default ({ name }) => <h1>Hello {name}!</h1>;\n"},{"title":"index.js","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"wRo98","is_binary":false,"id":"928871a1-bbdc-425c-ace2-0b302b14a58a","directory_shortid":"GXOoy","code":"import React from 'react';\nimport { render } from 'react-dom';\nimport Hello from './Hello';\n\nconst styles = {\n  fontFamily: 'sans-serif',\n  textAlign: 'center',\n};\n\nconst App = () => (\n  <div style={styles}>\n    <Hello name=\"CodeSandbox\" />\n    <h2>Start editing to see some magic happen {'\\u2728'}</h2>\n  </div>\n);\n\nrender(<App />, document.getElementById('root'));\n"},{"title":"index.html","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"BA1N","is_binary":false,"id":"9c54d8d0-5a0e-4e5f-8794-3092757733ee","directory_shortid":"rgkK4","code":"<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n\t<meta charset=\"utf-8\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n\t<meta name=\"theme-color\" content=\"#000000\">\n\t<!--\n      manifest.json provides metadata used when your web app is added to the\n      homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/\n    -->\n\t<link rel=\"manifest\" href=\"%PUBLIC_URL%/manifest.json\">\n\t<link rel=\"shortcut icon\" href=\"%PUBLIC_URL%/favicon.ico\">\n\t<!--\n      Notice the use of %PUBLIC_URL% in the tags above.\n      It will be replaced with the URL of the `public` folder during the build.\n      Only files inside the `public` folder can be referenced from the HTML.\n\n      Unlike \"/favicon.ico\" or \"favicon.ico\", \"%PUBLIC_URL%/favicon.ico\" will\n      work correctly both with client-side routing and a non-root public URL.\n      Learn how to configure a non-root public URL by running `npm run build`.\n    -->\n\t<title>React App</title>\n</head>\n\n<body>\n\t<noscript>\n\t\tYou need to enable JavaScript to run this app.\n\t</noscript>\n\t<div id=\"root\"></div>\n\t<!--\n      This HTML file is a template.\n      If you open it directly in the browser, you will see an empty page.\n\n      You can add webfonts, meta tags, or analytics to this file.\n      The build step will place the bundled scripts into the <body> tag.\n\n      To begin the development, run `npm start` or `yarn start`.\n      To create a production bundle, use `npm run build` or `yarn build`.\n    -->\n</body>\n\n</html>"}],"like_count":211,"id":"new","git":null,"forked_from_sandbox":null,"fork_count":0,"external_resources":[],"entry":"src/index.js","directories":[{"title":"src","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"GXOoy","id":"d27aefca-c15c-41a1-b9d5-bd362fdd7f19","directory_shortid":null},{"title":"public","source_id":"6bd39aa7-9800-45fb-9487-c915634d8d4f","shortid":"rgkK4","id":"859f77fe-8e09-4efb-bd44-f66ea4f949e4","directory_shortid":null}],"description":null,"author":null}})
 
-//   if (data.id) {
-//     return res.status(200).send({data})
-//   } else {
-//     return res.status(200).send({title:["No body or no directory"]});
-//   }
-// })
+  if (data.id) {
+    return res.status(200).send({data})
+  } else {
+    return res.status(200).send({title:["No body or no directory"]});
+  }
+})
 
 app.get("/api/v1/sandboxes/:sandboxID", upload.array(), async function (req, res) {
   console.log(`======Getting sandbox ${req.params.sandboxID} start=======`)
@@ -337,14 +338,40 @@ app.put("/api/v1/sandboxes/:sandboxID/modules/mupdate", upload.array(), async fu
 
 })
 
-
-
 app.post("/api/v1/copy", upload.array(), async function (req, res) {
   console.log(`======Making copy of sandbox at url start=======`)
   console.time('sandboxCopy')
   
 
   let { error, data } = await forkURL(req.body);
+
+  console.timeEnd('sandboxCopy')
+  console.log(`======Making copy of sandbox at url finish======\n`)
+  if (!error) {
+    return res.status(200).send({ok:true,instance:data})
+  } else {
+    return res.status(200).send({ok:false,error:data});
+  }
+
+})
+
+/*
+  {
+    displayName:,
+    url:,
+    type:"assignment,
+    id:,
+    index:,
+
+  }
+*/
+
+app.post("/api/v1/unfurl", upload.array(), async function (req, res) {
+  console.log(`======Making copy of sandbox at url start=======`)
+  console.time('sandboxCopy')
+  
+
+  let { error, data } = await unfurlURL(req.body);
 
   console.timeEnd('sandboxCopy')
   console.log(`======Making copy of sandbox at url finish======\n`)
